c9e0cfbeaa71f321d24e8d3fba975f11b88d1678
cleverapollo <cleverapollo@gmail.com>
2020-01-25 13:35:48 +0100

    add + configure angular-jwt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { IonicStorageModule } from '@ionic/storage';
+import { IonicStorageModule, Storage } from '@ionic/storage';
+import { JwtModule, JWT_OPTIONS } from "@auth0/angular-jwt";
 import { HttpClientModule } from '@angular/common/http';
+import { WishListResolver } from './home/wish-list.resolver';
+
+export function jwtOptionsFactory(storage) {
+  return {
+    tokenGetter: () => {
+      return storage.get('auth-token');
+    },
+    whitelistedDomains: ['127.0.0.1:8080'],
+    blacklistedRoutes: ['127.0.0.1:8080/auth*']
+  }
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,11 +31,19 @@ import { HttpClientModule } from '@angular/common/http';
     IonicModule.forRoot(),
     AppRoutingModule,
     IonicStorageModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    JwtModule.forRoot({
+      jwtOptionsProvider: {
+        provide: JWT_OPTIONS,
+        useFactory: jwtOptionsFactory,
+        deps: [Storage]
+      },
+    })
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    WishListResolver,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
