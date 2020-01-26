23396175cb84c95a71f33643c6c1ec7dafe81bf7
cleverapollo <cleverapollo@gmail.com>
2020-01-26 11:38:33 +0100

    adjust wish list model

--- src/app/home/wishlist.model.ts
+++ src/app/home/wishlist.model.ts
@@ -1,5 +1,13 @@
+export class Wish {
+    wishListId: Number;
+    name: String;
+    price: String;
+    productUrl: String;
+    imageUrl: String;
+}
+
 export class WishList {
     name: String
     date: String
-    imageUrl: String
+    wishes: Array<Wish>;
 }
