4020fcb72491132b6f176931999b5baab1396374
cleverapollo <cleverapollo@gmail.com>
2020-05-02 16:25:07 +0200

    adjust styles

--- src/app/registration/wish-list-wish/wish-list-wish.page.ts
+++ src/app/registration/wish-list-wish/wish-list-wish.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ValidationMessages, ValidationMessage } from 'src/app/shared/validation-messages/validation-message';
 
 @Component({
   selector: 'app-wish-list-wish',
@@ -9,14 +11,30 @@ import { NavController } from '@ionic/angular';
 })
 export class WishListWishPage implements OnInit {
 
-  keywords: String
+  form: FormGroup
+  validationMessages: ValidationMessages = {
+    keywords: [
+      new ValidationMessage('required', 'Gib bitte einen Suchbegriff ein.'),
+    ],
+  }
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private navController: NavController) { }
+  constructor(
+    private router: Router, 
+    private activatedRoute: ActivatedRoute, 
+    private navController: NavController,
+    private formBuilder: FormBuilder) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      'keywords': this.formBuilder.control(null, [Validators.required])
+    });
+  }
 
   search() {
-    this.router.navigate(['../search-results'], { relativeTo: this.activatedRoute, queryParams: { keywords: this.keywords } })
+    this.router.navigate(['../search-results'], { 
+      relativeTo: this.activatedRoute, 
+      queryParams: { keywords: this.form.get('keywords').value } 
+    });
   }
 
   goBack() {
