22e023ce3361a07d2ae37a6e3ac2d72d28d403b1
cleverapollo <cleverapollo@gmail.com>
2020-06-28 13:09:38 +0200

    fix protcol handling for gc images

--- src/app/shared/secure.pipe.ts
+++ src/app/shared/secure.pipe.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Pipe({
   name: 'secure'
@@ -17,7 +18,10 @@ export class SecurePipe implements PipeTransform {
         .set('Access-Control-Allow-Origin', '*')
       
       // FIXME: check why server sends http instead https
-      let modifiedUrl = (url as string).replace('http', 'https');
+      let modifiedUrl = url;
+      if (environment.production) {
+        modifiedUrl = (url as string).replace('http', 'https');
+      }
 
       return this.http
       .get(modifiedUrl, { responseType: 'blob', headers: headers }).pipe(
