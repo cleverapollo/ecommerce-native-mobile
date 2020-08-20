7f6ee403b4b8d80d415adb8948754d75f0ad3eeb
cleverapollo <cleverapollo@gmail.com>
2020-08-20 18:19:54 +0200

    improve caching mechanism for user profile

--- src/app/settings/user-profile-data.service.ts
+++ src/app/settings/user-profile-data.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { UserProfile } from '../shared/models/user.model';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Storage } from '@ionic/storage';
 import { Platform } from '@ionic/angular';
+import { UserApiService } from '../shared/api/user-api.service';
+import { map, publishReplay, refCount } from 'rxjs/operators';
 
 export interface StoredUserProfile {
   item: UserProfile;
@@ -15,20 +17,21 @@ export interface StoredUserProfile {
 export class UserProfileDataService {
 
   private STORAGE_KEY_USER_PROFILE = 'user-profile';
-
   private _userProfile: BehaviorSubject<UserProfile> = new BehaviorSubject(new UserProfile());
+  
+  userProfileData: Observable<UserProfile>;
+  userProfile$ = this._userProfile.asObservable();
 
-  constructor(private storage: Storage, private platform: Platform) { 
-    this.platform.ready().then(() => {
-      this.storage.get(this.STORAGE_KEY_USER_PROFILE).then((storedUserProfile: StoredUserProfile) => {
-        if (storedUserProfile) {
-          this._userProfile.next(storedUserProfile.item);
-        }
-      });
-    });
+  constructor(private storage: Storage, private platform: Platform, private api: UserApiService) {
+    this.initUserProfile();
   }
 
-  userProfile$ = this._userProfile.asObservable();
+  getUserProfile(): Observable<UserProfile> {
+    if (!this.userProfileData) {
+      this.userProfileData = this.getUserProfileFromServer()
+    }
+    return this.userProfileData;
+  }
 
   updateUserProfile(userProfile: UserProfile) {
     const itemToStore: StoredUserProfile = {
@@ -39,8 +42,38 @@ export class UserProfileDataService {
     this._userProfile.next(userProfile);
   }
 
-  clearUserProfile() {
+  clearCache() {
+    this.userProfileData = null;
     this.storage.remove(this.STORAGE_KEY_USER_PROFILE);
     this._userProfile.next(null);
+  }
+
+  private initUserProfile() {
+    this.platform.ready().then(() => {
+      this.getUserProfileFromCache().then((storedUserProfile: StoredUserProfile) => {
+        if (storedUserProfile) {
+          this._userProfile.next(storedUserProfile.item);
+        } else {
+          this.refreshFromServer();
+        }
+      });
+    });
+  }
+
+  private getUserProfileFromCache(): Promise<StoredUserProfile> {
+    return this.storage.get(this.STORAGE_KEY_USER_PROFILE);
+  }
+
+  private getUserProfileFromServer(): Observable<UserProfile> {
+    return this.api.getProfile().pipe(
+      publishReplay(1),
+      refCount()
+    );
+  }
+
+  private refreshFromServer() {
+    this.getUserProfileFromServer().subscribe(userProfile => {
+      this.updateUserProfile(userProfile);
+    });
   }
 }
--- src/app/shared/user-profile.resolver.ts
+++ src/app/shared/user-profile.resolver.ts
@@ -4,12 +4,13 @@ import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/r
 import { Observable } from 'rxjs';
 import { UserProfile } from './models/user.model';
 import { UserApiService } from './api/user-api.service';
+import { UserProfileDataService } from '../settings/user-profile-data.service';
 
 @Injectable()
 export class UserProfileResolver implements Resolve<Observable<UserProfile>> {
-  constructor(private userApiService: UserApiService) {}
+  constructor(private userProfileDataService: UserProfileDataService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.userApiService.getProfile();
+    return this.userProfileDataService.getUserProfile();
   }
 }
