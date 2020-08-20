f1e58d67338a5bcdb4863d7df5cbec2faf3cb4d0
cleverapollo <cleverapollo@gmail.com>
2020-08-20 17:46:53 +0200

    make response visible for user (update user image)

--- src/app/settings/pages/profile-image-update/profile-image-update.page.ts
+++ src/app/settings/pages/profile-image-update/profile-image-update.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserApiService } from 'src/app/shared/api/user-api.service';
 import { UserProfile } from 'src/app/shared/models/user.model';
 import { ActivatedRoute } from '@angular/router';
+import { HintConfig } from 'src/app/shared/hint/hint.component';
+import { UserProfileDataService } from '../../user-profile-data.service';
 
 @Component({
   selector: 'app-profile-image-update',
@@ -12,8 +14,14 @@ export class ProfileImageUpdatePage implements OnInit {
 
   profile: UserProfile
   imageIsUploading: Boolean = false;
+  showHint: Boolean;
+  hintConfig: HintConfig
 
-  constructor(private route: ActivatedRoute, private userApiService: UserApiService) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private userApiService: UserApiService, 
+    private userProfileDataService: UserProfileDataService) 
+    { }
 
   ngOnInit() {
     this.profile = this.route.snapshot.data.profile;
@@ -24,21 +32,41 @@ export class ProfileImageUpdatePage implements OnInit {
     const reader = new FileReader();
     reader.readAsArrayBuffer(file);
     reader.onload = () => {
-
       let request = new FormData();
       request.append('file', file);
       this.imageIsUploading = true;
-
       this.userApiService.uploadFile(request).subscribe((response) => {
-        this.profile.profileImageUrl = response.fileDownloadUri;
-        this.profile.profileImageFileName = response.fileName;
-      }, e => console.error, () => {
+        this.updateProfile(response.fileDownloadUri, response.fileName);
+        this.hintConfig = new HintConfig("success", "Dein Profilbild wurde erfolgreich hochgeladen!");
+      }, e => {
+        console.error(e);
+        this.hintConfig = new HintConfig("danger", "Aufgrund eines Fehlers konnte dein Profilbild nicht hochgeladen werden!")
+      }, () => {
         this.imageIsUploading = false;
+        this.makeHintVisible()
       });
     }
     reader.onerror = (error) => {
       console.error(error);
+      this.hintConfig = new HintConfig("danger", "Aufgrund eines Fehlers konnte dein Profilbild nicht hochgeladen werden!")
+      this.makeHintVisible()
     }
+  }
+
+  private makeHintVisible() {
+    this.showHint = true;
+    setTimeout(() => {
+      this.showHint = false;
+    }, 3000);
+  }
+
+  private updateProfile(url: String, fileName: String) {
+    this.userProfileDataService.userProfile$.toPromise().then( profile => {
+      profile.profileImageUrl = url;
+      profile.profileImageFileName = fileName;
+      this.userProfileDataService.updateUserProfile(profile);
+      this.profile = profile;
+    })
   }
 
   deleteImage() {
