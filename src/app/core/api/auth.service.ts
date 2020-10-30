8dcce140ca1a05779ee47545dbba249c3a01d1e4
cleverapollo <cleverapollo@gmail.com>
2020-10-30 18:33:03 +0100

    add error messages for registration

--- src/app/core/api/auth.service.ts
+++ src/app/core/api/auth.service.ts
@@ -60,10 +60,42 @@ export class AuthService {
   }
 
   register(dto: RegistrationDto) : Observable<LoginResponse> {
-    return this.apiService.post(`${AuthService.REST_END_POINT}/register`, dto) as Observable<LoginResponse>;
+    return this.apiService.post<LoginResponse>(`${AuthService.REST_END_POINT}/register`, dto).pipe(
+      catchError(error => this.handleRegistrationError(error))
+    );
   }
 
   registerPartner(dto: RegistrationPartnerDto) : Observable<any> {
-    return  this.apiService.post(`${AuthService.REST_END_POINT}/register-partner`, dto);
+    return  this.apiService.post(`${AuthService.REST_END_POINT}/register-partner`, dto).pipe(
+      catchError(error => this.handleRegistrationError(error))
+    );
+  }
+
+  private handleRegistrationError(error: any) {
+    let errorMessage = this.errorMessageForRegistrationResponse(error);
+    this.toastService.presentErrorToast(errorMessage);
+    return throwError(error);
+  }
+
+  private errorMessageForRegistrationResponse(error: any) {
+    let errorMessage = 'Ein allgemeiner Fehler ist aufgetreten, bitte versuche es später noch einmal.';
+    if (error instanceof HttpErrorResponse) {
+      if (error.error instanceof ErrorEvent) {
+        console.log(`Error: ${error.error.message}`);
+      } else {
+        console.log(`error status : ${error.status} ${error.statusText}`);
+        errorMessage = this.errorMessageForRegistrationServerError(error, errorMessage);
+      }
+    }
+    return errorMessage;
+  }
+
+  private errorMessageForRegistrationServerError(error: HttpErrorResponse, errorMessage: string) {
+    switch (error.status) {
+      case 409:
+        errorMessage = 'Ein Benutzer mit der eingegeben E-Mail-Adresse existiert bereits!';
+        break;
+    }
+    return errorMessage;
   }
 }
