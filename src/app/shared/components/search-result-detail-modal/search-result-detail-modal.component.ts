ddff832513d4e31f88972e014be21d6d11180be6
cleverapollo <cleverapollo@gmail.com>
2020-10-31 14:34:54 +0100

    WANTIC-84 add missing browser navigation

--- src/app/shared/components/search-result-detail-modal/search-result-detail-modal.component.ts
+++ src/app/shared/components/search-result-detail-modal/search-result-detail-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { SearchResultItem } from '@core/models/search-result-item';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-search-result-detail-modal',
@@ -11,7 +12,7 @@ export class SearchResultDetailModalComponent {
 
   @Input() searchResultItem: SearchResultItem
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private inAppBrowser: InAppBrowser) { }
   
   dismiss() {
     this.modalController.dismiss();
@@ -21,6 +22,12 @@ export class SearchResultDetailModalComponent {
     this.modalController.dismiss({
       selectWish: true
     });
+  }
+
+  openProductURL() {
+    const url = this.searchResultItem.productUrl
+    const browser = this.inAppBrowser.create(url);
+    browser.show();
   }
 
 }
