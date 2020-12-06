aaaf3240dc46986c5985f0292992cd5ea727d4ea
cleverapollo <cleverapollo@gmail.com>
2020-12-06 16:01:25 +0100

    add missing import

--- src/app/menu/settings/pages/account-delete/account-delete.module.ts
+++ src/app/menu/settings/pages/account-delete/account-delete.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { AccountDeletePageRoutingModule } from './account-delete-routing.module';
 
 import { AccountDeletePage } from './account-delete.page';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     AccountDeletePageRoutingModule
   ],
   declarations: [AccountDeletePage]
