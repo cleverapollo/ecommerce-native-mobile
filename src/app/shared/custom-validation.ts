9beebd827dcedc6ca572241a3e0dcfdf5c30f3db
cleverapollo <cleverapollo@gmail.com>
2020-12-07 19:29:12 +0100

    fix validator

--- src/app/shared/custom-validation.ts
+++ src/app/shared/custom-validation.ts
@@ -40,9 +40,12 @@ export class CustomValidation {
   }
 
   static email(control: AbstractControl): ValidationErrors | null {
-    const emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-    const validEmailAdress = emailRegex.test(control.value);
-    return validEmailAdress ? null : { email: control.value };
+    if (control.value?.length > 0) {
+      const emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+      const validEmailAdress = emailRegex.test(control.value);
+      return validEmailAdress ? null : { email: control.value };
+    }
+    return null;
   }
 
 }
