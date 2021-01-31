a02e71296abf95a50398212ae208319bbaa173e7
cleverapollo <cleverapollo@gmail.com>
2021-01-31 10:52:06 +0100

    change log level

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -33,7 +33,7 @@ export class StorageService {
               resolve(null);
             }
           }, error => {
-            this.logger.error(storageKey, error);
+            this.logger.debug(storageKey, error);
             resolve(null);
           });
         } else {
@@ -44,7 +44,7 @@ export class StorageService {
               resolve(null);
             }
           }, error => {
-            this.logger.error(storageKey, error);
+            this.logger.debug(storageKey, error);
             resolve(null);
           })
         }
