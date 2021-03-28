d2e60f87ad2ac471aab470032705df475c90ceca
cleverapollo <cleverapollo@gmail.com>
2021-03-28 14:07:26 +0200

    adjusted error handling

--- src/assets/scripts/parse-imgs-from-website.js
+++ src/assets/scripts/parse-imgs-from-website.js
@@ -2,13 +2,13 @@ Array.from(document.getElementsByTagName('img')).map(x => {
     if (x.src) { 
         let imageSrcUrl = x.src;
         let imageUrl = '';
-
-        if (imageSrcUrl.match(/https/g).length === 1 || imageSrcUrl.match(/http/g).length === 1) {
+        console.log(imageSrcUrl);
+        if (imageSrcUrl.match(/https/g)?.length === 1 || imageSrcUrl.match(/http/g)?.length === 1) {
             const urlToCheck = imageSrcUrl.split(/[?#]/)[0];
             if (urlToCheck.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
                 imageUrl = imageSrcUrl;
             }
-        } else if (imageSrcUrl.match(/https/g).length > 1) {
+        } else if (imageSrcUrl.match(/https/g)?.length > 1) {
             let urls = imageSrcUrl.split('https');
             urls.forEach(url => {
                 url = 'https' + url;
@@ -17,7 +17,7 @@ Array.from(document.getElementsByTagName('img')).map(x => {
                     imageUrl = url;
                 }
             })
-        } else if (imageSrcUrl.match(/http/g).length > 1) {
+        } else if (imageSrcUrl.match(/http/g)?.length > 1) {
             let urls = imageSrcUrl.split('http');
             urls.forEach(url => {
                 url = 'http' + url;
@@ -26,12 +26,12 @@ Array.from(document.getElementsByTagName('img')).map(x => {
                     imageUrl = url;
                 }
             })
-        }
-        return { name: x.alt, imageUrl: imageUrl };  
+        } 
+        return { name: x.alt, imageUrl: imageUrl || '' };  
     } else if(x.srcset) {
         const imagesUrls = Array.from(x.srcset.split(','))
             .filter(url => url.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null)
-        return { name: x.alt, imageUrl: imagesUrls[0] }; 
+        return { name: x.alt, imageUrl: imagesUrls[0] || '' }; 
     } 
     return { name: '', imageUrl: ''  };  
 }).filter(x => x.imageUrl !== "" && x.imageUrl.startsWith('http'));
