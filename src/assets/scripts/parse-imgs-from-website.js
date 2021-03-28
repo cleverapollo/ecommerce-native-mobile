b1c34f36a8e84dc58dffb3d308443b853dde2d5d
cleverapollo <cleverapollo@gmail.com>
2021-03-28 14:19:49 +0200

    adjusted parsing script for product images

--- src/assets/scripts/parse-imgs-from-website.js
+++ src/assets/scripts/parse-imgs-from-website.js
@@ -1,14 +1,8 @@
 Array.from(document.getElementsByTagName('img')).map(x => { 
     if (x.src) { 
         let imageSrcUrl = x.src;
-        let imageUrl = '';
-        console.log(imageSrcUrl);
-        if (imageSrcUrl.match(/https/g)?.length === 1 || imageSrcUrl.match(/http/g)?.length === 1) {
-            const urlToCheck = imageSrcUrl.split(/[?#]/)[0];
-            if (urlToCheck.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
-                imageUrl = imageSrcUrl;
-            }
-        } else if (imageSrcUrl.match(/https/g)?.length > 1) {
+        let imageUrl = imageSrcUrl;
+        if (imageSrcUrl.match(/https/g)?.length > 1) {
             let urls = imageSrcUrl.split('https');
             urls.forEach(url => {
                 url = 'https' + url;
