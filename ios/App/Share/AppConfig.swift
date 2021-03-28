bdba4155d1c24ecb37b38bf39ff44fbdbae0c834
cleverapollo <cleverapollo@gmail.com>
2021-03-28 17:33:42 +0200

    configured "Wantic Beta" target

---
+++ ios/App/Share/AppConfig.swift
@@ -0,0 +1,15 @@
+//
+//  AppConfig.swift
+//  Share
+//
+//  Created by Tim Fischer on 28.03.21.
+//
+
+import Foundation
+
+struct AppConfig {
+    
+    static let appUrl = "https://app.wantic.io"
+    static let backendUrl = "https://rest-prd.wantic.io"
+    
+}
