b7a8b001344b62b21ec817c2a0d0a5fe8ed243cb
cleverapollo <cleverapollo@gmail.com>
2021-03-28 13:59:53 +0200

    fixed script

--- src/assets/scripts/parse-imgs-from-website.js
+++ src/assets/scripts/parse-imgs-from-website.js
@@ -5,7 +5,7 @@ Array.from(document.getElementsByTagName('img')).map(x => {
 
         if (imageSrcUrl.match(/https/g).length === 1 || imageSrcUrl.match(/http/g).length === 1) {
             const urlToCheck = imageSrcUrl.split(/[?#]/)[0];
-            if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
+            if (urlToCheck.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
                 imageUrl = imageSrcUrl;
             }
         } else if (imageSrcUrl.match(/https/g).length > 1) {
@@ -13,7 +13,7 @@ Array.from(document.getElementsByTagName('img')).map(x => {
             urls.forEach(url => {
                 url = 'https' + url;
                 const urlToCheck = url.split(/[?#]/)[0];
-                if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
+                if (urlToCheck.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
                     imageUrl = url;
                 }
             })
@@ -22,7 +22,7 @@ Array.from(document.getElementsByTagName('img')).map(x => {
             urls.forEach(url => {
                 url = 'http' + url;
                 const urlToCheck = url.split(/[?#]/)[0];
-                if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
+                if (urlToCheck.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
                     imageUrl = url;
                 }
             })
@@ -30,7 +30,7 @@ Array.from(document.getElementsByTagName('img')).map(x => {
         return { name: x.alt, imageUrl: imageUrl };  
     } else if(x.srcset) {
         const imagesUrls = Array.from(x.srcset.split(','))
-            .filter(url => url.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null)
+            .filter(url => url.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null)
         return { name: x.alt, imageUrl: imagesUrls[0] }; 
     } 
     return { name: '', imageUrl: ''  };  
