7777c44007f2c215975fc7bab44e22146deae85d
cleverapollo <cleverapollo@gmail.com>
2021-03-28 13:50:38 +0200

    WANTIC-266 adjusted img parsing script

--- src/assets/scripts/parse-imgs-from-website.js
+++ src/assets/scripts/parse-imgs-from-website.js
@@ -1,8 +1,37 @@
 Array.from(document.getElementsByTagName('img')).map(x => { 
     if (x.src) { 
-        return { name: x.alt, imageUrl: x.src };  
-    } else if(x.srcset) { 
-        return { name: x.alt, imageUrl: x.srcset.split(',')[0] }; 
+        let imageSrcUrl = x.src;
+        let imageUrl = '';
+
+        if (imageSrcUrl.match(/https/g).length === 1 || imageSrcUrl.match(/http/g).length === 1) {
+            const urlToCheck = imageSrcUrl.split(/[?#]/)[0];
+            if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
+                imageUrl = imageSrcUrl;
+            }
+        } else if (imageSrcUrl.match(/https/g).length > 1) {
+            let urls = imageSrcUrl.split('https');
+            urls.forEach(url => {
+                url = 'https' + url;
+                const urlToCheck = url.split(/[?#]/)[0];
+                if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
+                    imageUrl = url;
+                }
+            })
+        } else if (imageSrcUrl.match(/http/g).length > 1) {
+            let urls = imageSrcUrl.split('http');
+            urls.forEach(url => {
+                url = 'http' + url;
+                const urlToCheck = url.split(/[?#]/)[0];
+                if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
+                    imageUrl = url;
+                }
+            })
+        }
+        return { name: x.alt, imageUrl: imageUrl };  
+    } else if(x.srcset) {
+        const imagesUrls = Array.from(x.srcset.split(','))
+            .filter(url => url.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null)
+        return { name: x.alt, imageUrl: imagesUrls[0] }; 
     } 
     return { name: '', imageUrl: ''  };  
 }).filter(x => x.imageUrl !== "" && x.imageUrl.startsWith('http'));
