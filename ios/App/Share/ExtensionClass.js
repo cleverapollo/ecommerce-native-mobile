522a61f5981d2fcc8add5804c894a52aa1c933a7
cleverapollo <cleverapollo@gmail.com>
2021-06-22 20:23:23 +0200

    WANTIC-290 fixed image parsing

--- ios/App/Share/ExtensionClass.js
+++ ios/App/Share/ExtensionClass.js
@@ -17,8 +17,8 @@ ExtensionClass.prototype = {
                 .map(elem => elem.href)[0];
     },
     fetchProductInfos: function() {
-        return Array.from(document.getElementsByTagName('img')).map(x => { 
-            if (x.src) { 
+        return Array.from(document.getElementsByTagName('img')).map(x => {
+            if (x.src) {
                 let imageSrcUrl = x.src;
                 let imageUrl = imageSrcUrl;
                 if (imageSrcUrl.match(/https/g)?.length > 1) {
@@ -26,8 +26,8 @@ ExtensionClass.prototype = {
                     urls.forEach(url => {
                         url = 'https' + url;
                         const urlToCheck = url.split(/[?#]/)[0];
-                        if (urlToCheck.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
-                            imageUrl = url;
+                        if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
+                            imageUrl = encodeURI(urlToCheck);
                         }
                     })
                 } else if (imageSrcUrl.match(/http/g)?.length > 1) {
@@ -35,19 +35,21 @@ ExtensionClass.prototype = {
                     urls.forEach(url => {
                         url = 'http' + url;
                         const urlToCheck = url.split(/[?#]/)[0];
-                        if (urlToCheck.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
-                            imageUrl = url;
+                        if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
+                            imageUrl = encodeURI(urlToCheck);
                         }
                     })
-                } 
+                } else if(x.srcset) {
+                    return getFirstImageUrlFromSrcSet(x)
+                } else if (imageSrcUrl.startsWith('//') && imageSrcUrl.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
+                    imageUrl = adjustImageUrl(imageSrcUrl);
+                }
                 return { name: x.alt, imageUrl: imageUrl || '' };  
             } else if(x.srcset) {
-                const imagesUrls = Array.from(x.srcset.split(','))
-                    .filter(url => url.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null)
-                return { name: x.alt, imageUrl: imagesUrls[0] || '' }; 
+                return getFirstImageUrlFromSrcSet(x)
             } 
             return { name: '', imageUrl: ''  };  
-        }).filter(x => x.imageUrl !== "" && x.imageUrl.startsWith('http'));
+        }).filter(x => x.imageUrl !== "" && (x.imageUrl.startsWith('http') || x.imageUrl.startsWith('//')));
     },
     fetchPrice: function() {
         let elements = document.body.getElementsByTagName('*');
@@ -79,7 +81,10 @@ ExtensionClass.prototype = {
             } 
         });
         const resultPrice = mode(prices);
-        return parseFloat(resultPrice.replace('€', '').replace(',', '.').trim()) ?? 0.00;
+        if (resultPrice) {
+            return parseFloat(resultPrice.replace('€', '').replace(',', '.').trim()) ?? 0.00;
+        }
+        return 0.00;
     }
 };
 
@@ -96,6 +101,24 @@ function mode(arr){
           arr.filter(v => v===a).length
         - arr.filter(v => v===b).length
     ).pop();
+}
+
+function getFirstImageUrlFromSrcSet(x) {
+    const imagesUrls = Array.from(x.srcset.split(','))
+        .filter(url => url.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null);
+    let imageUrl = '';
+    if (imagesUrls.length >= 1) {
+        imageUrl = adjustImageUrl(imagesUrls[0])
+    }
+    return { name: x.alt, imageUrl: imageUrl };
+}
+
+function adjustImageUrl(imageUrl) {
+    let adjustedUrl = imageUrl.split(' ')[0];
+    if (adjustedUrl.startsWith('//')) {
+        adjustedUrl = 'https:' + adjustedUrl;
+    }
+    return encodeURI(adjustedUrl);
 }
 
 var ExtensionPreprocessingJS = new ExtensionClass;
--- src/assets/scripts/parse-imgs-from-website.js
+++ src/assets/scripts/parse-imgs-from-website.js
@@ -1,5 +1,5 @@
-Array.from(document.getElementsByTagName('img')).map(x => { 
-    if (x.src) { 
+Array.from(document.getElementsByTagName('img')).map(x => {
+    if (x.src) {
         let imageSrcUrl = x.src;
         let imageUrl = imageSrcUrl;
         if (imageSrcUrl.match(/https/g)?.length > 1) {
@@ -7,8 +7,8 @@ Array.from(document.getElementsByTagName('img')).map(x => {
             urls.forEach(url => {
                 url = 'https' + url;
                 const urlToCheck = url.split(/[?#]/)[0];
-                if (urlToCheck.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
-                    imageUrl = url;
+                if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
+                    imageUrl = encodeURI(urlToCheck);
                 }
             })
         } else if (imageSrcUrl.match(/http/g)?.length > 1) {
@@ -16,16 +16,18 @@ Array.from(document.getElementsByTagName('img')).map(x => {
             urls.forEach(url => {
                 url = 'http' + url;
                 const urlToCheck = url.split(/[?#]/)[0];
-                if (urlToCheck.match(/(jpeg|jpg|svg|png|tiff|tif|gif)$/) != null) {
-                    imageUrl = url;
+                if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
+                    imageUrl = encodeURI(urlToCheck);
                 }
             })
         } else if(x.srcset) {
-            return getFirstImageUrlFromSrcSet(x); 
-        } 
+            return getFirstImageUrlFromSrcSet(x)
+        } else if (imageSrcUrl.startsWith('//') && imageSrcUrl.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
+            imageUrl = adjustImageUrl(imageSrcUrl);
+        }
         return { name: x.alt, imageUrl: imageUrl || '' };  
     } else if(x.srcset) {
-        return getFirstImageUrlFromSrcSet(x);  
+        return getFirstImageUrlFromSrcSet(x)
     } 
     return { name: '', imageUrl: ''  };  
 }).filter(x => x.imageUrl !== "" && (x.imageUrl.startsWith('http') || x.imageUrl.startsWith('//')));
@@ -33,5 +35,17 @@ Array.from(document.getElementsByTagName('img')).map(x => {
 function getFirstImageUrlFromSrcSet(x) {
     const imagesUrls = Array.from(x.srcset.split(','))
         .filter(url => url.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null);
-    return { name: x.alt, imageUrl: imagesUrls[0] || '' };
+    let imageUrl = '';
+    if (imagesUrls.length >= 1) {
+        imageUrl = adjustImageUrl(imagesUrls[0])
+    }
+    return { name: x.alt, imageUrl: imageUrl };
+}
+
+function adjustImageUrl(imageUrl) {
+    let adjustedUrl = imageUrl.split(' ')[0];
+    if (adjustedUrl.startsWith('//')) {
+        adjustedUrl = 'https:' + adjustedUrl;
+    }
+    return encodeURI(adjustedUrl);
 }
