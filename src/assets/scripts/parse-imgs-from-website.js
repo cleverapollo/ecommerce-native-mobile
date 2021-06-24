88a1c67f3137729765f76d1695943de08b349629
cleverapollo <cleverapollo@gmail.com>
2021-06-24 18:39:11 +0200

    WANTIC-290 adjusted script

--- src/assets/scripts/parse-imgs-from-website.js
+++ src/assets/scripts/parse-imgs-from-website.js
@@ -1,29 +1,16 @@
 Array.from(document.getElementsByTagName('img')).map(x => {
     if (x.src) {
         let imageSrcUrl = x.src;
-        let imageUrl = imageSrcUrl;
+        let imageUrl = getPathFromUrl(imageSrcUrl);
         if (imageSrcUrl.match(/https/g)?.length > 1) {
-            let urls = imageSrcUrl.split('https');
-            urls.forEach(url => {
-                url = 'https' + url;
-                const urlToCheck = url.split(/[?#]/)[0];
-                if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
-                    imageUrl = encodeURI(urlToCheck);
-                }
-            })
+            imageUrl = validateImageUrl(imageSrcUrl, 'https');
         } else if (imageSrcUrl.match(/http/g)?.length > 1) {
-            let urls = imageSrcUrl.split('http');
-            urls.forEach(url => {
-                url = 'http' + url;
-                const urlToCheck = url.split(/[?#]/)[0];
-                if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
-                    imageUrl = encodeURI(urlToCheck);
-                }
-            })
+            imageUrl = validateImageUrl(imageSrcUrl, 'http');
         } else if(x.srcset) {
             return getFirstImageUrlFromSrcSet(x)
-        } else if (imageSrcUrl.startsWith('//') && imageSrcUrl.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
-            imageUrl = adjustImageUrl(imageSrcUrl);
+        } else if (imageSrcUrl.startsWith('//')) {
+            imageSrcUrl = 'https' + imageSrcUrl;
+            imageUrl = validateImageUrl(imageSrcUrl, 'https');
         }
         return { name: x.alt, imageUrl: imageUrl || '' };  
     } else if(x.srcset) {
@@ -31,6 +18,20 @@ Array.from(document.getElementsByTagName('img')).map(x => {
     } 
     return { name: '', imageUrl: ''  };  
 }).filter(x => x.imageUrl !== "" && (x.imageUrl.startsWith('http') || x.imageUrl.startsWith('//')));
+
+function validateImageUrl(imageSrcUrl, protocol) {
+    let imageUrl = imageSrcUrl;
+    let urls = imageSrcUrl.split(protocol);
+    urls.forEach(url => {
+        const urlToCheck = protocol + url;
+        if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
+            imageUrl = encodeURI(urlToCheck);
+        } else {
+            imageUrl = '';
+        }
+    });
+    return imageUrl;
+}
 
 function getFirstImageUrlFromSrcSet(x) {
     const imagesUrls = Array.from(x.srcset.split(','))
@@ -47,5 +48,10 @@ function adjustImageUrl(imageUrl) {
     if (adjustedUrl.startsWith('//')) {
         adjustedUrl = 'https:' + adjustedUrl;
     }
+    adjustedUrl = getPathFromUrl(adjustedUrl);
     return encodeURI(adjustedUrl);
 }
+
+function getPathFromUrl(url) {
+    return url.split("?")[0];
+  }
