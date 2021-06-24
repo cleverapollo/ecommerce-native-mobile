9ca0f79b78ab1957335e596d0e325105e102010e
cleverapollo <cleverapollo@gmail.com>
2021-06-24 19:52:21 +0200

    WANTIC-290 adjusted extension class

--- ios/App/Share/ExtensionClass.js
+++ ios/App/Share/ExtensionClass.js
@@ -19,37 +19,17 @@ ExtensionClass.prototype = {
     fetchProductInfos: function() {
         return Array.from(document.getElementsByTagName('img')).map(x => {
             if (x.src) {
-                let imageSrcUrl = x.src;
-                let imageUrl = imageSrcUrl;
-                if (imageSrcUrl.match(/https/g)?.length > 1) {
-                    let urls = imageSrcUrl.split('https');
-                    urls.forEach(url => {
-                        url = 'https' + url;
-                        const urlToCheck = url.split(/[?#]/)[0];
-                        if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
-                            imageUrl = encodeURI(urlToCheck);
-                        }
-                    })
-                } else if (imageSrcUrl.match(/http/g)?.length > 1) {
-                    let urls = imageSrcUrl.split('http');
-                    urls.forEach(url => {
-                        url = 'http' + url;
-                        const urlToCheck = url.split(/[?#]/)[0];
-                        if (urlToCheck.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
-                            imageUrl = encodeURI(urlToCheck);
-                        }
-                    })
-                } else if(x.srcset) {
-                    return getFirstImageUrlFromSrcSet(x)
-                } else if (imageSrcUrl.startsWith('//') && imageSrcUrl.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null) {
-                    imageUrl = adjustImageUrl(imageSrcUrl);
+                let imageUrl = validateUrl(x.src);
+                if (x.srcset) {
+                    imageUrl = getFirstImageUrlFromSrcSet(x.srcset);
                 }
                 return { name: x.alt, imageUrl: imageUrl || '' };  
-            } else if(x.srcset) {
-                return getFirstImageUrlFromSrcSet(x)
+            } else if (x.srcset) {
+                const imageUrl = getFirstImageUrlFromSrcSet(x.srcset);
+                return { name: x.alt, imageUrl: imageUrl || '' };
             } 
             return { name: '', imageUrl: ''  };  
-        }).filter(x => x.imageUrl !== "" && (x.imageUrl.startsWith('http') || x.imageUrl.startsWith('//')));
+        }).filter(x => x.imageUrl !== '' && (x.imageUrl.startsWith('http')));
     },
     fetchPrice: function() {
         let elements = document.body.getElementsByTagName('*');
@@ -103,22 +83,20 @@ function mode(arr){
     ).pop();
 }
 
-function getFirstImageUrlFromSrcSet(x) {
-    const imagesUrls = Array.from(x.srcset.split(','))
-        .filter(url => url.match(/\.(jpeg|jpg|svg|png|tiff|tif|gif)/g) != null);
+function validateUrl(url) {
+    let imageUrl = url;
+    if (!url.startsWith('http') && url.startsWith('//')) {
+        imageUrl = 'https:' + url; 
+    }
+    return encodeURI(imageUrl);
+}
+
+function getFirstImageUrlFromSrcSet(srcset) {
+    const imagesUrls = Array.from(srcset.split(','));
     let imageUrl = '';
     if (imagesUrls.length >= 1) {
-        imageUrl = adjustImageUrl(imagesUrls[0])
+        imageUrl = validateUrl(imagesUrls[0])
     }
-    return { name: x.alt, imageUrl: imageUrl };
+    return imageUrl;
 }
-
-function adjustImageUrl(imageUrl) {
-    let adjustedUrl = imageUrl.split(' ')[0];
-    if (adjustedUrl.startsWith('//')) {
-        adjustedUrl = 'https:' + adjustedUrl;
-    }
-    return encodeURI(adjustedUrl);
-}
-
 var ExtensionPreprocessingJS = new ExtensionClass;
