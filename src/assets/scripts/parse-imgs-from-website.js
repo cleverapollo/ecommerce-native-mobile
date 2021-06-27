cf6473f595731bc8b1034e0960b8d751a218a993
cleverapollo <cleverapollo@gmail.com>
2021-06-27 12:26:37 +0200

    WANTIC-158 updated script

--- src/assets/scripts/parse-imgs-from-website.js
+++ src/assets/scripts/parse-imgs-from-website.js
@@ -10,14 +10,28 @@ Array.from(document.getElementsByTagName('img')).map(x => {
         return { name: x.alt, imageUrl: imageUrl || '' };
     } 
     return { name: '', imageUrl: ''  };  
-}).filter(x => x.imageUrl !== '' && (x.imageUrl.startsWith('http')));
+}).filter(x => x.imageUrl !== '' && (x.imageUrl.startsWith('http')))
+.sort(sortBySvgImages);
 
 function validateUrl(url) {
     let imageUrl = url;
     if (!url.startsWith('http') && url.startsWith('//')) {
         imageUrl = 'https:' + url; 
     }
+    imageUrl = prepareImageUrlIfNeeded(imageUrl)
     return encodeURI(imageUrl);
+}
+
+function prepareImageUrlIfNeeded(url) {
+    const validImageTypes = ['.jpeg', ,'.jpg','.svg','.png','.tiff','.tif','.gif'];
+    for (const type of validImageTypes) {
+        const indexOfTypeString = url.indexOf(type);
+        if (indexOfTypeString !== -1) {
+            url = url.substring(0, indexOfTypeString + type.length);
+            break;
+        }
+    }
+    return url;
 }
 
 function getFirstImageUrlFromSrcSet(srcset) {
@@ -27,4 +41,17 @@ function getFirstImageUrlFromSrcSet(srcset) {
         imageUrl = validateUrl(imagesUrls[0])
     }
     return imageUrl;
+}
+
+function sortBySvgImages(a, b) {
+    const isASvgImage = a.imageUrl.indexOf('.svg') !== -1;
+    const isBSvgImage = b.imageUrl.indexOf('.svg') !== -1;
+    if (isASvgImage && isBSvgImage) {
+        return 0;
+    } else if (isASvgImage) {
+        return 1;
+    } else if (isBSvgImage) {
+        return -1;
+    }
+    return 0;
 }
