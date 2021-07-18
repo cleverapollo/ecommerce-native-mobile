79e47734cd65d34f694dfd3df6ba325badb679b7
cleverapollo <cleverapollo@gmail.com>
2021-07-18 11:54:44 +0200

    WANTIC-49 configured first app build

--- android/app/src/main/java/io/wantic/app/MainActivity.java
+++ android/app/src/main/java/io/wantic/app/MainActivity.java
@@ -1,6 +1,11 @@
 package io.wantic.app;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.ServiceWorkerClient;
+import android.webkit.ServiceWorkerController;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
@@ -17,5 +22,17 @@ public class MainActivity extends BridgeActivity {
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
+
+    if(Build.VERSION.SDK_INT >= 24 ){
+      ServiceWorkerController swController = ServiceWorkerController.getInstance();
+
+      swController.setServiceWorkerClient(new ServiceWorkerClient() {
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebResourceRequest request) {
+          return bridge.getLocalServer().shouldInterceptRequest(request);
+        }
+      });
+    }
+
   }
 }
