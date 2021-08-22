1d1f43be474af4e9e4196cdf31f4be6f36a71f5f
cleverapollo <cleverapollo@gmail.com>
2021-08-22 12:11:23 +0200

    WANTIC-333 added vertical align to slides + refactoring

--- src/app/wish-lists/wish-search-selection/onboarding-slides/onboarding-slides.component.ts
+++ src/app/wish-lists/wish-search-selection/onboarding-slides/onboarding-slides.component.ts
@@ -6,6 +6,13 @@ import { UserProfileStore } from '@menu/settings/user-profile-store.service';
 import { first } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { PlatformService } from '@core/services/platform.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+class WanticSlideItem {
+  imgSrc: string;
+  imgAlt: string;
+  description: SafeHtml
+}
 
 @Component({
   selector: 'app-onboarding-slides',
@@ -19,13 +26,78 @@ export class OnboardingSlidesComponent implements OnInit {
   showBackButton: boolean = false;
   showCompleteButton: boolean = false;
 
+  get slidesItems(): Array<WanticSlideItem> {
+    let items = [];
+    if (this.platformService.isAndroid) {
+      items = this.androidSlides;
+    } else if (this.platformService.isIOS) {
+      items = this.iOSSlides;
+    }
+    return items;
+  };
+
   constructor(
     private modalController: ModalController,
     private userApiService: UserApiService,
     private logger: LogService,
     private userProfileStore: UserProfileStore,
+    private domSanitzer: DomSanitizer,
     public platformService: PlatformService
   ) { }
+
+  private get androidSlides(): WanticSlideItem[] {
+    return [
+      {
+        imgSrc: `assets/images/onboarding-slide-android-1.png`,
+        imgAlt: 'Onboarding Slide 1', // ToDo: improve description
+        description: this.domSanitzer.bypassSecurityTrustHtml(`Springe in deinen Browser und 
+          füge Wünsche aus deinem liebsten 
+          Onlineshop über dieses Icon <ion-icon color="primary" name="ellipsis-vertical-outline"></ion-icon> hinzu.`)
+      },
+      {
+        imgSrc: `assets/images/onboarding-slide-android-2.png`,
+        imgAlt: 'Onboarding Slide 2', // ToDo: improve description
+        description: this.domSanitzer.bypassSecurityTrustHtml(`Drücke dann auf Teilen <ion-icon color="primary" name="share-social-outline"></ion-icon>, 
+          um unsere App auf deinem 
+          Smartphone auswählen zu können.`)
+      },
+      {
+        imgSrc: `assets/images/onboarding-slide-android-3.png`,
+        imgAlt: 'Onboarding Slide 3', // ToDo: improve description
+        description: this.domSanitzer.bypassSecurityTrustHtml(`Gehe in deinem App-Menü bis ganz nach  
+          rechts & wähle unter &bdquo;Mehr&ldquo; wantic aus. `)
+      },
+      {
+        imgSrc: `assets/images/onboarding-slide-android-4.png`,
+        imgAlt: 'Onboarding Slide 4', // ToDo: improve description
+        description: this.domSanitzer.bypassSecurityTrustHtml(`Klicke auf das Wantic-Symbol & speichere deinen Wunsch auf deiner Wunschliste.`)
+      },
+    ];
+  }
+
+  private get iOSSlides(): WanticSlideItem[] { 
+    return [
+      {
+        imgSrc: 'assets/images/onboarding-slide-1.png',
+        imgAlt: 'Onboarding Slide 1', // ToDo: improve description
+        description: this.domSanitzer.bypassSecurityTrustHtml(`Springe in deinen Browser und <br>
+        füge Wünsche aus deinem liebsten <br>
+        Onlineshop über dieses Icon <ion-icon class="share-icon" name="share-outline"></ion-icon> hinzu.`)
+      },
+      {
+        imgSrc: 'assets/images/onboarding-slide-2.png',
+        imgAlt: 'Onboarding Slide 2', // ToDo: improve description
+        description: this.domSanitzer.bypassSecurityTrustHtml(`Wische bei den App-Favoriten nach rechts <br>
+        und füge unter "Mehr" Wantic hinzu.<br><br>`)
+      },
+      {
+        imgSrc: 'assets/images/onboarding-slide-3.png',
+        imgAlt: 'Onboarding Slide 3', // ToDo: improve description
+        description: this.domSanitzer.bypassSecurityTrustHtml(`Klicke auf das Wantic-Symbol und füge<br>
+        deinen Wunsch zu deiner Liste hinzu.<br><br>`)
+      },
+    ]
+  }
 
   ngOnInit() {}
 
