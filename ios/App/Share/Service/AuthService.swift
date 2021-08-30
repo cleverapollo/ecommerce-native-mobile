f2d2543e960b989f4cb45557c14b7fab8b07cf8d
cleverapollo <cleverapollo@gmail.com>
2021-08-30 19:20:55 +0200

    configured firebase auth

--- ios/App/Share/Service/AuthService.swift
+++ ios/App/Share/Service/AuthService.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftKeychainWrapper
 import JWTDecode
+import Firebase
 
 class AuthService {
     
@@ -15,13 +16,53 @@ class AuthService {
     
     static let shared = AuthService()
     
-    private init() {}
+    private init() {
+        if FirebaseApp.app() == nil {
+            configureFirebaseApp()
+            do {
+                try Auth.auth().useUserAccessGroup(AppConfig.keychainAccessGroup)
+            } catch let error as NSError {
+                print("Error changing user acccess group %@", error)
+            }
+        }
+    }
+    
+    private func configureFirebaseApp() {
+        if let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") {
+            if let firebaseOptions = FirebaseOptions(contentsOfFile: filePath) {
+                FirebaseApp.configure(options: firebaseOptions)
+            } else {
+                print("Error creating FirebaseOptions")
+            }
+        } else {
+            print("GoogleService-Info.plist was not found")
+        }
+    }
     
     func getAuthToken(completionHandler: @escaping (String?) -> Void) {
-        if let encodedToken = keychainwrapper.string(forKey: "firebaseIdToken") {
-            completionHandler(encodedToken.replacingOccurrences(of: "\"", with: ""))
-        } else {
+        getIdToken() { idToken in
+            if let idToken = idToken {
+                completionHandler(idToken)
+            } else if let encodedToken = self.keychainwrapper.string(forKey: "firebaseIdToken") {
+                completionHandler(encodedToken.replacingOccurrences(of: "\"", with: ""))
+            } else {
+                completionHandler(nil)
+            }
+        }
+
+    }
+    
+    private func getIdToken(completionHandler: @escaping (String?) -> Void) {
+        guard let currentUser = Auth.auth().currentUser else {
             completionHandler(nil)
+            return
+        }
+        currentUser.getIDToken() { idToken, error in
+            guard let idToken = idToken, error == nil else {
+                completionHandler(nil)
+                return
+            }
+            completionHandler(idToken)
         }
     }
     
