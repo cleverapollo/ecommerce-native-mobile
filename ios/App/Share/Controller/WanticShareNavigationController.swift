536f2a2507d5eeac7e9a22b92a9c82cb8be58d80
cleverapollo <cleverapollo@gmail.com>
2021-11-03 19:35:31 +0100

    fixed navbar color for ios 15 devices

--- ios/App/Share/Controller/WanticShareNavigationController.swift
+++ ios/App/Share/Controller/WanticShareNavigationController.swift
@@ -9,5 +9,15 @@ import UIKit
 
 class WanticShareNavigationController: UINavigationController {
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if #available(iOSApplicationExtension 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .white
+            navigationBar.standardAppearance = appearance;
+            navigationBar.scrollEdgeAppearance = navigationBar.standardAppearance
+        }
+    }
 }
