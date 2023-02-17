f4a91f6622182e8b7b3b9462b848e78656b8f664
cleverapollo <cleverapollo@gmail.com>
2023-02-17 22:06:59 +0100

    fix: Fix image crawling for mobile.de
    Move test data to files.

--- android/app/src/test/java/io/wantic/app/share/utils/ShareResultParserTest.kt
+++ android/app/src/test/java/io/wantic/app/share/utils/ShareResultParserTest.kt
@@ -4,6 +4,7 @@ import io.wantic.app.share.models.ShareResult
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import java.io.File
 
 class ShareResultParserTest {
 
@@ -33,7 +34,8 @@ class ShareResultParserTest {
     fun parseTextToShareResult_URLandName_returnsInitializedModelWithURLStringAndName() {
         val content = "Hoola Bronzer Mini https://www.douglas.de/de/p/3001030751"
         val result = shareResultParser.parseTextToShareResult(content)
-        val expectedResult = ShareResult("https://www.douglas.de/de/p/3001030751", "Hoola Bronzer Mini")
+        val expectedResult =
+            ShareResult("https://www.douglas.de/de/p/3001030751", "Hoola Bronzer Mini")
         Assert.assertEquals(expectedResult, result)
     }
 
@@ -46,15 +48,31 @@ class ShareResultParserTest {
 
     @Test
     fun parseTextToShareResult_multipleUrls_returnsFirstOne() {
-        val content = "Schau Dir dieses Produkt bei ABOUT YOU an:\n" +
-                "    \n" +
-                "    https://m.aboutyou.de/p/nike-sportswear/sneaker-air-max-270-5840320?utm_medium=referral&utm_source=onsite_sharing&utm_campaign=product\n" +
-                "    \n" +
-                "    Lade Dir jetzt die App herunter: \n" +
-                "    \n" +
-                "    https://app.adjust.com/hdfs9w4?deep_link=appaboutyou://p/nike-sportswear/sneaker-air-max-270-5840320";
+        val content = loadTxt("about-you.txt")
         val result = shareResultParser.parseTextToShareResult(content)
-        val expectedResult = ShareResult("https://m.aboutyou.de/p/nike-sportswear/sneaker-air-max-270-5840320?utm_medium=referral&utm_source=onsite_sharing&utm_campaign=product", "Schau Dir dieses Produkt bei ABOUT YOU an:")
+        val expectedResult = ShareResult(
+            "https://m.aboutyou.de/p/nike-sportswear/sneaker-air-max-270-5840320?utm_medium=referral&utm_source=onsite_sharing&utm_campaign=product",
+            "Schau Dir dieses Produkt bei ABOUT YOU an:"
+        )
         Assert.assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun parseTextToShareResult_textWithSpecialChars_returnsLink() {
+        val content = loadTxt("mobile-shared.txt")
+        val result = shareResultParser.parseTextToShareResult(content)
+        val expectedResult = ShareResult(
+            "https://link.mobile.de/v66Cw8ByBP4wzpM87",
+            loadTxt("mobile-expected.txt")
+        )
+        Assert.assertEquals(expectedResult, result)
+    }
+
+    private fun loadTxt(fileName: String): String {
+        val resource = javaClass.classLoader?.getResource(fileName);
+        Assert.assertNotNull(resource)
+        val file = File(resource!!.path)
+        Assert.assertNotNull(file)
+        return file.readText()
     }
 }
