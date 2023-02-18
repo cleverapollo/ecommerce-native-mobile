616aa110144ac7de76ce719a1f5c3a5d6b7af369
cleverapollo <cleverapollo@gmail.com>
2023-02-18 17:32:54 +0100

    refactor: Format code and organize imports

--- android/app/src/main/java/io/wantic/app/MainActivity.java
+++ android/app/src/main/java/io/wantic/app/MainActivity.java
@@ -1,7 +1,5 @@
 package io.wantic.app;
 
-import com.getcapacitor.community.firebaseanalytics.FirebaseAnalytics;
-
 import android.os.Bundle;
 import android.webkit.ServiceWorkerClient;
 import android.webkit.ServiceWorkerController;
@@ -9,19 +7,20 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.firebaseanalytics.FirebaseAnalytics;
 
 public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    registerPlugin(FirebaseAnalytics.class);
-    super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(FirebaseAnalytics.class);
+        super.onCreate(savedInstanceState);
 
-    ServiceWorkerController swController = ServiceWorkerController.getInstance();
-    swController.setServiceWorkerClient(new ServiceWorkerClient() {
-      @Override
-      public WebResourceResponse shouldInterceptRequest(WebResourceRequest request) {
-        return bridge.getLocalServer().shouldInterceptRequest(request);
-      }
-    });
-  }
+        ServiceWorkerController swController = ServiceWorkerController.getInstance();
+        swController.setServiceWorkerClient(new ServiceWorkerClient() {
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebResourceRequest request) {
+                return bridge.getLocalServer().shouldInterceptRequest(request);
+            }
+        });
+    }
 }
