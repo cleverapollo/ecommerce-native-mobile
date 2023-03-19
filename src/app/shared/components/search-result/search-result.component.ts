6fb130c14812343d8a6c5bc9ae44b82de4cb2b81
cleverapollo <cleverapollo@gmail.com>
2023-03-19 16:14:17 +0100

    style: Fix height for images

--- src/app/shared/components/search-result/search-result.component.ts
+++ src/app/shared/components/search-result/search-result.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SearchResultItem } from '@core/models/search-result-item';
 import { BrowserService } from '@core/services/browser.service';
+import { WishImageComponentStyles } from '../wish-image/wish-image.component';
 
 @Component({
   selector: 'app-search-result',
@@ -13,6 +14,18 @@ export class SearchResultComponent {
   @Output() selectWish = new EventEmitter<SearchResultItem>();
 
   constructor(private browserService: BrowserService) { }
+
+  get imgStyles(): WishImageComponentStyles {
+    return {
+      img: {
+        'padding-top': '8px',
+        'max-height': 'inherit'
+      },
+      container: {
+        'max-height': 'inherit'
+      }
+    }
+  }
 
   addWish() {
     this.selectWish.emit(this.wish);
