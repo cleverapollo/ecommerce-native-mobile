77a2e181e0a03e9b9c49a3e762b6c2168c14cc49
cleverapollo <cleverapollo@gmail.com>
2023-09-26 19:52:10 +0200

    feat: Make list shareable

--- src/app/shared/components/list/list.component.ts
+++ src/app/shared/components/list/list.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Share } from '@capacitor/share';
 import { Product, ProductList } from '@core/models/product-list.model';
 import { WishDto, WishListDto } from '@core/models/wish-list.model';
+import { Logger } from '@core/services/log.service';
+import { APP_URL } from '@env/environment';
+import { UserProfileStore } from '@menu/settings/user-profile-store.service';
 import { WishImageComponentStyles } from '../wish-image/wish-image.component';
 
 @Component({
@@ -12,6 +16,8 @@ export class ListComponent {
 
   @Input() list: ProductList | WishListDto;
   @Input() index: number;
+
+  @Output() listSelected = new EventEmitter<ProductList | WishListDto>();
 
   get side(): string {
     if (this.index % 2 === 0) {
@@ -43,6 +49,10 @@ export class ListComponent {
     return this.list['wishes'] !== undefined;
   }
 
+  get isProductList(): boolean {
+    return this.list['products'] !== undefined;
+  }
+
   get items(): WishDto[] | Product[] {
     return this.isWishList ?
       (this.list as WishListDto).wishes || [] :
@@ -51,6 +61,25 @@ export class ListComponent {
 
   get date(): Date | null {
     return this.isWishList ? (this.list as WishListDto).date : null;
+  }
+
+  constructor(private logger: Logger, private userStore: UserProfileStore) { }
+
+  share() {
+    const userName = this.userStore.user$.value.creatorAccount?.userName;
+    const message = `Folge der Liste "${this.list.name}" von @${userName} auf wantic und lass dich inspirieren! 🥳🎁🤩`;
+    const link = `${APP_URL}/creator/${userName}/${this.list.name}`;
+    Share.share({
+      title: `👉 Folge der Liste "${this.list.name}" von @${userName}`,
+      text: message,
+      url: link
+    }).catch(reason => {
+      this.logger.error(link, reason);
+    });
+  }
+
+  onListSelected() {
+    this.listSelected.emit(this.list);
   }
 
 }
