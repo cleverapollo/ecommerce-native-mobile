a69110f78f940b88a490809a1cfffeb289504177
cleverapollo <cleverapollo@gmail.com>
2023-10-15 14:05:46 +0200

    refactor: Replace deprecated methods

--- src/app/core/api/content-creator-api.service.ts
+++ src/app/core/api/content-creator-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ContentCreatorAccount, NewCreator, SocialMediaLinks } from '@core/models/content-creator.model';
 import { WanticError } from '@core/models/error.model';
-import { Observable, throwError } from 'rxjs';
+import { Observable, lastValueFrom, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { ApiVersion } from './api-version';
 import { ApiService } from './api.service';
@@ -18,62 +18,62 @@ export class ContentCreatorApiService {
 
   createAccount(data: NewCreator): Observable<string> {
     return this.apiService.postRaw<void>(`${ApiVersion.v1}/${ContentCreatorApiService.REST_END_POINT}`, data).pipe(
-      catchError(error => throwError(new WanticError(error))),
+      catchError(error => throwError(() => new WanticError(error))),
       map(response => response.headers.get('Location'))
     );
   }
 
   getAccountByUserName(userName: string): Observable<ContentCreatorAccount> {
     return this.apiService.get<ContentCreatorAccount>(`${ApiVersion.v1}/${ContentCreatorApiService.PUBLIC_REST_END_POINT}/${userName}/`).pipe(
-      catchError(error => throwError(new WanticError(error)))
+      catchError(error => throwError(() => new WanticError(error)))
     );
   }
 
   searchForAccounts(searchTerm: string): Observable<ContentCreatorAccount[]> {
     return this.apiService.get<ContentCreatorAccount[]>(`${ApiVersion.v1}/${ContentCreatorApiService.PUBLIC_REST_END_POINT}?searchTerm=${searchTerm}`).pipe(
-      catchError(error => throwError(new WanticError(error)))
+      catchError(error => throwError(() => new WanticError(error)))
     );
   }
 
   updateName(name: string): Observable<ContentCreatorAccount> {
     return this.apiService.patch<ContentCreatorAccount>(this._buildUrl('name'), { name }).pipe(
-      catchError(error => throwError(new WanticError(error)))
+      catchError(error => throwError(() => new WanticError(error)))
     );
   }
 
   updateUserName(newUserName: string): Observable<ContentCreatorAccount> {
     return this.apiService.patch<ContentCreatorAccount>(this._buildUrl('user-name'), { userName: newUserName }).pipe(
-      catchError(error => throwError(new WanticError(error)))
+      catchError(error => throwError(() => new WanticError(error)))
     );
   }
 
   updateDescription(description: string): Observable<ContentCreatorAccount> {
     return this.apiService.patch<ContentCreatorAccount>(this._buildUrl('description'), { description }).pipe(
-      catchError(error => throwError(new WanticError(error)))
+      catchError(error => throwError(() => new WanticError(error)))
     );
   }
 
   updateSocialMediaLinks(socialMediaLinks: SocialMediaLinks): Observable<ContentCreatorAccount> {
     return this.apiService.patch<ContentCreatorAccount>(this._buildUrl('social-media-links'), socialMediaLinks).pipe(
-      catchError(error => throwError(new WanticError(error)))
+      catchError(error => throwError(() => new WanticError(error)))
     );
   }
 
   getImage(): Observable<Blob> {
     return this.apiService.downloadFile(this._buildUrl('image')).pipe(
-      catchError(error => throwError(new WanticError(error)))
+      catchError(error => throwError(() => new WanticError(error)))
     );
   }
 
   getImageByUserName(userName: string): Observable<Blob> {
     return this.apiService.downloadFile(this._buildPublicUrl(userName, 'image')).pipe(
-      catchError(error => throwError(new WanticError(error)))
+      catchError(error => throwError(() => new WanticError(error)))
     );
   }
 
   async updateImage(file: ArrayBuffer): Promise<void> {
     try {
-      return await this.apiService.uploadFile<void>(this._buildUrl('image'), file).toPromise();
+      return await lastValueFrom(this.apiService.uploadFile<void>(this._buildUrl('image'), file));
     } catch (error) {
       throw new WanticError(error);
     }
@@ -81,7 +81,7 @@ export class ContentCreatorApiService {
 
   deleteImage(): Observable<ContentCreatorAccount> {
     return this.apiService.delete<ContentCreatorAccount>(this._buildUrl('image')).pipe(
-      catchError(error => throwError(new WanticError(error)))
+      catchError(error => throwError(() => new WanticError(error)))
     );
   }
 
