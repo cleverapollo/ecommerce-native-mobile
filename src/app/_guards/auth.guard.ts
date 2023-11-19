1842bc80fe299cfd8724f9875869118383e3cb68
cleverapollo <cleverapollo@gmail.com>
2023-11-19 12:54:15 +0100

    refactor: fix auto login handling for mock env

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { AuthenticationService } from '@core/services/authentication.service';
+import { BackendConfigType } from '@env/backend-config-type';
+import { environment } from '@env/environment';
 import { Observable } from 'rxjs';
 import { filter, map, take } from 'rxjs/operators';
 
@@ -12,11 +14,12 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthenticationService) { }
 
   canActivate(): Observable<boolean> {
+    const isMock = !environment.production && environment.backendType === BackendConfigType.dev;
     return this.authService.isAuthenticated.pipe(
       filter(isAuthenticated => { return isAuthenticated !== null }),
       take(1),
       map(isAuthenticated => {
-        return true;
+        if (isMock) { return true; }
         if (isAuthenticated === null) {
           return false;
         }
--- src/app/_guards/auto-login.guard.ts
+++ src/app/_guards/auto-login.guard.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
 import { AuthenticationService } from '@core/services/authentication.service';
 import { Logger } from '@core/services/log.service';
+import { BackendConfigType } from '@env/backend-config-type';
+import { environment } from '@env/environment';
 import { UserProfileStore } from '@menu/settings/user-profile-store.service';
 import { Observable } from 'rxjs';
 import { filter, map, take } from 'rxjs/operators';
@@ -19,14 +21,15 @@ export class AutoLoginGuard implements CanLoad {
   ) { }
 
   canLoad(): Observable<boolean> {
+    const isMock = !environment.production && environment.backendType === BackendConfigType.dev;
     return this.authService.isAuthenticated.pipe(
       filter(isAuthenticated => isAuthenticated !== null),
       take(1),
       map(isAuthenticated => {
-        /*if (!isAuthenticated) {
+        if (!isMock && !isAuthenticated) {
           this.logger.info('no auto login');
           return true;
-        }*/
+        }
         this.logger.info('auto login');
         this.router.navigateByUrl('/secure/home', { replaceUrl: true });
         return false;
